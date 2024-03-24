bb42ab7307933a6e26848ae642921c2438f52308
alikus10 <kusali938@gmail.com>
2024-03-24 04:03:33 +0300

    ana sayfa, üye ekleme , kitap ekleme, emanet işlemleri için formlar oluşturuldu.

--- kütüphane1/kütüphane_ana_sayfa.cs
+++ kütüphane1/kütüphane_ana_sayfa.cs
@@ -12,24 +12,33 @@ namespace kütüphane1
 {
     public partial class kütüphane_ana_sayfa : Form
     {
+        private Üye_ekle Üye_ekle;
+        private Kitap_ekle Kitap_ekle;
+        private Emanet Emanet;
         public kütüphane_ana_sayfa()
         {
             InitializeComponent();
+
+            Üye_ekle = new Üye_ekle();
+            Kitap_ekle = new Kitap_ekle();
+            Emanet = new Emanet();
         }
 
         private void üye_ekle_btn_Click(object sender, EventArgs e)
         {
 
+            Üye_ekle.Show();
+           
         }
 
         private void kitap_ekle_btn_Click(object sender, EventArgs e)
         {
-
+            Kitap_ekle.Show();
         }
 
         private void emanet_btn_Click(object sender, EventArgs e)
         {
-
+            Emanet.Show();
         }
     }
 }
