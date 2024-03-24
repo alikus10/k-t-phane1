5b0679e4ca66ba83d5392a3fd11711aec838c42d
alikus10 <kusali938@gmail.com>
2024-03-24 03:42:43 +0300

    Proje dosyası ekle.

---
+++ kütüphane1/kütüphane_ana_sayfa.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace kütüphane1
+{
+    partial class kütüphane_ana_sayfa
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.üye_ekle_btn = new System.Windows.Forms.Button();
+            this.kitap_ekle_btn = new System.Windows.Forms.Button();
+            this.emanet_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // üye_ekle_btn
+            // 
+            this.üye_ekle_btn.Location = new System.Drawing.Point(95, 62);
+            this.üye_ekle_btn.Name = "üye_ekle_btn";
+            this.üye_ekle_btn.Size = new System.Drawing.Size(131, 77);
+            this.üye_ekle_btn.TabIndex = 0;
+            this.üye_ekle_btn.Text = "Üye Ekle";
+            this.üye_ekle_btn.UseVisualStyleBackColor = true;
+            this.üye_ekle_btn.Click += new System.EventHandler(this.üye_ekle_btn_Click);
+            // 
+            // kitap_ekle_btn
+            // 
+            this.kitap_ekle_btn.Location = new System.Drawing.Point(95, 163);
+            this.kitap_ekle_btn.Name = "kitap_ekle_btn";
+            this.kitap_ekle_btn.Size = new System.Drawing.Size(131, 77);
+            this.kitap_ekle_btn.TabIndex = 0;
+            this.kitap_ekle_btn.Text = "Kitap Ekle";
+            this.kitap_ekle_btn.UseVisualStyleBackColor = true;
+            this.kitap_ekle_btn.Click += new System.EventHandler(this.kitap_ekle_btn_Click);
+            // 
+            // emanet_btn
+            // 
+            this.emanet_btn.Location = new System.Drawing.Point(95, 266);
+            this.emanet_btn.Name = "emanet_btn";
+            this.emanet_btn.Size = new System.Drawing.Size(131, 77);
+            this.emanet_btn.TabIndex = 0;
+            this.emanet_btn.Text = "Emanet İşlemleri";
+            this.emanet_btn.UseVisualStyleBackColor = true;
+            this.emanet_btn.Click += new System.EventHandler(this.emanet_btn_Click);
+            // 
+            // kütüphane_ana_sayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 403);
+            this.Controls.Add(this.emanet_btn);
+            this.Controls.Add(this.kitap_ekle_btn);
+            this.Controls.Add(this.üye_ekle_btn);
+            this.Name = "kütüphane_ana_sayfa";
+            this.Text = "Ana Sayfa";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button üye_ekle_btn;
+        private System.Windows.Forms.Button kitap_ekle_btn;
+        private System.Windows.Forms.Button emanet_btn;
+    }
+}
+
---
+++ kütüphane1/kütüphane_ana_sayfa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kütüphane1
+{
+    public partial class kütüphane_ana_sayfa : Form
+    {
+        public kütüphane_ana_sayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void üye_ekle_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kitap_ekle_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void emanet_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
